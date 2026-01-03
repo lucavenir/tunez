9a6047f31f09a618cc27c6ac94b93c9516caf433
~venir <luca@venir.dev>
2026-01-03 10:48:15 +0100

    relationships w/ validations and uniqueness!

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -22,6 +22,23 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  validations do
+    validate numericality(
+               :year_released,
+               greater_than_or_equal_to: 1889,
+               less_than_or_equal_to: &__MODULE__.next_year/0
+             ),
+             where: [present(:year_released)],
+             message: "must be between 1889 and the next year"
+
+    validate match(
+               :cover_image_url,
+               ~r"^(https://|/images/).+(\.png|\.jpg)$"
+             ),
+             where: [changing(:cover_image_url)],
+             message: "must start with https:// or /images/, and must end with .png or .jpg"
+  end
+
   attributes do
     uuid_v7_primary_key :id
 
@@ -35,4 +52,11 @@ defmodule Tunez.Music.Album do
   relationships do
     belongs_to :artist, Tunez.Music.Artist, allow_nil?: false
   end
+
+  identities do
+    identity :unique_album_per_artist, [:name, :artist_id],
+      message: "an album with this name already exists for this artist"
+  end
+
+  def next_year, do: Date.utc_today().year + 2
 end
